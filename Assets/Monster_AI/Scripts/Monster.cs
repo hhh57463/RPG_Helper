1e3d9cac0b52de797db052f2fb9393a717624e62
hhh57463 <hhh57463@naver.com>
2023-03-05 17:02:01 +0900

    Feat: 몬스터 AI

---
+++ Assets/Monster_AI/Scripts/Monster.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [Tooltip("[Player Script]")]
+    [SerializeField] Player player;
+    [Tooltip("[Monster Type]\n1: In Range Move Monster\n2: Esacape Range, revert point Monster\n3: In Range Follow Monster")]
+    [SerializeField] int monsterType;
+    [Tooltip("[Monster Patern]\n0: Comback SpawnPoint\n1: Stand\n2: Move\n3: Follow Player")]
+    [SerializeField] int patern;
+    [SerializeField] float speed;
+    float moveSpeed;
+    Vector3 spawnPoint;
+    Vector3 targetPos;
+
+    float spawnRadius;
+
+    void Start()
+    {
+        spawnPoint = transform.localPosition;
+        speed = 11.0f;
+        spawnRadius = 30f;
+        StartCoroutine("Thinking");
+    }
+
+    void Update()
+    {
+        switch (monsterType)
+        {
+            case 1:
+                MonsterType1();
+                break;
+            case 2:
+                MonsterType2();
+                break;
+            case 3:
+                MonsterType3();
+                break;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StopCoroutine("Thinking");
+            transform.localPosition = spawnPoint;
+            speed = 11.0f;
+            StartCoroutine("Thinking");
+        }
+    }
+
+    void MonsterType1()
+    {
+        if (Vector3.Distance(spawnPoint, transform.localPosition) >= spawnRadius)
+        {
+            StopCoroutine("Thinking");
+            patern = 0;
+        }
+
+        PaternAction();
+
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPos, moveSpeed * Time.deltaTime);
+        if (transform.localPosition == targetPos)
+            MovePos();
+    }
+
+    float chaseRadius = 30f;
+    void MonsterType2()
+    {
+        if (Vector3.Distance(spawnPoint, transform.localPosition) >= spawnRadius)
+        {
+            StopCoroutine("Thinking");
+            patern = 0;
+        }
+        if (Vector3.Distance(spawnPoint, player.transform.position) <= chaseRadius)
+        {
+            StopCoroutine("Thinking");
+            patern = 3;
+        }
+
+        PaternAction();
+
+        if (patern == 3)
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        else
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPos, moveSpeed * Time.deltaTime);
+        if (transform.localPosition == targetPos)
+            MovePos();
+    }
+
+    bool isChasing = false;
+    void MonsterType3()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) <= chaseRadius)
+        {
+            Debug.Log("Chase");
+            StopCoroutine("Thinking");
+            isChasing = true;
+            patern = 3;
+        }
+        if (isChasing && Vector3.Distance(player.transform.position, transform.position) >= chaseRadius)
+        {
+            if (Vector3.Distance(spawnPoint, transform.localPosition) >= spawnRadius)
+            {
+                Debug.Log("Revert");
+                isChasing = false;
+                patern = 0;
+            }
+        }
+        PaternAction();
+        if (patern == 3)
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        else
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPos, moveSpeed * Time.deltaTime);
+        if (transform.localPosition == targetPos)
+            MovePos();
+    }
+
+    IEnumerator Thinking()
+    {
+        SelectPatern();
+        yield return new WaitForSeconds(10.0f);
+        StartCoroutine("Thinking");
+    }
+
+    void MovePos()
+    {
+        targetPos = new Vector3(Random.Range(-25, 25), 1.5f, Random.Range(-25, 25));
+        transform.LookAt(targetPos);
+    }
+
+    void SelectPatern()
+    {
+        patern = Random.Range(2, 3);
+        if (patern == 2)
+            MovePos();
+    }
+
+    void PaternAction()
+    {
+        switch (patern)
+        {
+            case 0:
+                targetPos = spawnPoint;
+                moveSpeed = speed;
+                if (Vector3.Distance(spawnPoint, transform.localPosition) >= 1.0f)
+                    StartCoroutine("Thinking");
+                break;
+            case 1:
+                moveSpeed = 0f;
+                break;
+            case 2:
+                moveSpeed = speed;
+                break;
+            case 3:
+                targetPos = player.transform.position;
+                break;
+        }
+    }
+}
---
+++ Assets/Monster_AI/Scripts/Player.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    CharacterController controller;
+
+    Vector3 moveDir;
+
+    [Header("Player State")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpPower;
+    [SerializeField] float rotSpeed;
+    float gravity;
+    float viewDirX;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        speed = 10.0f;
+        jumpPower = 5.0f;
+        gravity = 10.0f;
+        rotSpeed = 2.0f;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    float h => Input.GetAxis("Horizontal");
+    float v => Input.GetAxis("Vertical");
+
+    void Move()
+    {
+        viewDirX += Input.GetAxis("Mouse X") * rotSpeed;
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector3(h, 0f, v);
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDir.y = jumpPower;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+                speed = 20.0f;
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+                speed = 10.0f;
+        }
+        transform.rotation = Quaternion.Euler(0f, viewDirX, 0f);
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+    }
+}
